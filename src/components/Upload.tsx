0334aa7104a7c1079e8231f9d17b635c4e81e3ba
vilchy <dawid.drobny@zs6sobieski.edu.pl>
2024-04-05 16:30:25 +0200

    Updated ui on button

--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -149,7 +149,7 @@ export default function Upload() {
         form={form}
         className="mb-8 inline-flex h-10 w-full max-w-3xl items-center justify-center whitespace-nowrap rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground ring-offset-background transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
       >
-        Add post
+        Dodaj Post
       </SubmitButton>
     </>
   );
